7f8cceec10fdb7a0949c4ae5c57ea34377607090
Raditya Dahal <dahalraditya5511@gmail.com>
2026-05-09 00:51:55 +0300

    Fix invalid URL error in Playwright tests

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -3,13 +3,13 @@ const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('')
+    await page.goto('http://127.0.0.1:5001')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 
   test('pokemon page can be navigated to', async ({ page }) => {
-    await page.goto('')
+    await page.goto('http://127.0.0.1:5001')
     await page.getByText('ivysaur').click()
     await expect(page.getByText('chlorophyll')).toBeVisible()
   })
